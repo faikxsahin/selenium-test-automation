4b95ef2874f227870d7b8f89b78c3cd974a4807d
faikxsahin <faikxsahin@outlook.com>
2026-01-27 14:02:39 +0300

    Added classes and test automation code for successful login

---
+++ src/main/java/com/practicetestautomation/pageobjects/SuccessfulLoginPage.java
@@ -0,0 +1,22 @@
+package com.practicetestautomation.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SuccessfulLoginPage extends BasePage {
+
+    private By logOutButtonLocator = By.linkText("Log out");
+
+    public SuccessfulLoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isLogOutButtonDisplayed() {
+        return isDisplayed(logOutButtonLocator);
+    }
+
+    public void load() {
+        waitForElement(logOutButtonLocator);
+    }
+
+}
--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -1,4 +1,47 @@
 package com.practicetestautomation.tests.login;
 
+import com.practicetestautomation.pageobjects.LoginPage;
+import com.practicetestautomation.pageobjects.SuccessfulLoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTests {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void positiveLoginTest() {
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.visit();
+
+        SuccessfulLoginPage successfulLoginPage = loginPage.executeLogin("student", "Password123");
+
+        successfulLoginPage.load();
+
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = successfulLoginPage.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        String expectedText = "Congratulations student. You successfully logged in!";
+        String pageSource = successfulLoginPage.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedText));
+
+        Assert.assertTrue(successfulLoginPage.isLogOutButtonDisplayed());
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
 }
