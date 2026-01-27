8cf3876d600ce1757f6c9c2e63e8b73b18d38f4e
faikxsahin <faikxsahin@outlook.com>
2026-01-27 15:17:20 +0300

    Added Logger for test information

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -4,6 +4,7 @@ import com.practicetestautomation.pageobjects.LoginPage;
 import com.practicetestautomation.pageobjects.SuccessfulLoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,14 +13,18 @@ import org.testng.annotations.Test;
 public class LoginTests {
 
     private WebDriver driver;
+    private Logger logger;
 
     @BeforeMethod
     public void setup() {
         driver = new ChromeDriver();
+        logger.info("Running test in Chrome browser!");
     }
 
     @Test
     public void positiveLoginTest() {
+        logger.info("Starting positiveLoginTest");
+
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.visit();
@@ -32,6 +37,8 @@ public class LoginTests {
         String actualUrl = successfulLoginPage.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl);
 
+        logger.info("Verifying login...");
+
         String expectedText = "Congratulations student. You successfully logged in!";
         String pageSource = successfulLoginPage.getPageSource();
         Assert.assertTrue(pageSource.contains(expectedText));
@@ -42,6 +49,7 @@ public class LoginTests {
     @AfterMethod
     public void teardown() {
         driver.quit();
+        logger.info("Browser is closed!");
     }
 
 }
